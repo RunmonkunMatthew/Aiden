6889a6a0ea0d63f366c82c333a8aa5a53ade4243
RunmonkunMatthew <runmonkunmatthew@gmail.com>
2025-12-19 03:31:24 -0800

    proxy update

--- client/src/types.ts
+++ client/src/types.ts
@@ -3,3 +3,21 @@ export type User = {
   email: string | null,
   chats: object,
 }
+
+
+export type Sender = 'user' | 'bot'
+
+export type Chat = {
+  id?: string,
+  createdAt: number,
+  title: string,
+  messages: object
+}
+
+export type Message = {
+  id?: string,
+  sender: Sender,
+  text: string,
+  createdAt: number,
+  severity?: "normal" | "warning" | "emergency"
+}
