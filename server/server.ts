a609852447db766f46225ce41b8d20120628ba8b
RunmonkunMatthew <runmonkunmatthew@gmail.com>
2026-01-09 05:48:10 -0800

    Proxy update

--- server/server.ts
+++ server/server.ts
@@ -1,50 +1,77 @@
-// const path = require('path')
-const express = require('express')
+import express from 'express';
 import type {Request, Response} from 'express'
+import axios from 'axios';
+import cors from 'cors';
+require('dotenv').config();
 
-const axios = require('axios')
-require('dotenv').config()
-const port = process.env.PORT
+const {isEmergency, detectScenario} = require('./utils')
+const {CORE_SYSTEM_PROMPT, SCENARIOS } = require('./prompt')
 
 const app = express()
-const SYSTEM_PROMPT = require('./prompt')
-app.use(express.json())
+const port = process.env.PORT || 5000
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({message: 'Welcome to aiden', result: 'Done', console:process.env.GEMINI_API_KEY})
+})
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-  try {
-    const {prompt} = req.body
+  const { message } = req.body;
+  const API_URL ='https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
 
-    const response = await axios.post(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent", 
-          {
-      contents: [
-      {
-        role: "user",
-        parts: [{ text: prompt }]
-    }
-    ]}, 
 
-    {
-      params: {
-      key: process.env.GEMINI_API_KEY
-      }
-    }
-
-    );
-
-    res.json(response.data)
-    console.log(response.data);
-    
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({error: 'AI request failed'})
+  if (isEmergency(message)) {
+    return res.json({
+      reply: "This may be a medical emergency. Please contact emergency services immediately",
+      emergency: true
+    });
   }
-})
+
+  const scenario = detectScenario(message);
+  const systemPrompt = `${CORE_SYSTEM_PROMPT}\n${SCENARIOS[scenario]}`;
+  
+
+  try {
+    const response = await axios.post(`${API_URL}?key=${process.env.GEMINI_API_KEY}`, {
+      contents: [
+        {
+          role: 'user',
+          parts: [{ text: `${systemPrompt}\n\nuser:${message}` }]
+        }
+      ]
+    });
+
+    const aiReply = response.data.candidates?.[0]?.content?.parts?.[0]?.text || "I'm sorry, I couldn't process that.";
+    
+    res.json({
+      aiReply,
+      scenario,
+      emergency: false
+    });
+
+  } catch (error: any) {
+    console.error("AI Error:", error.response?.data || error.message);
+    res.status(500).json({ error: 'AI service failed' });
+  }
+
+});
 
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 })
 
-
-
+// server.on('error', (err) => {
+//   console.error("Server error:", err);
+// });
